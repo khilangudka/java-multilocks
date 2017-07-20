858b64442b79f7aefc757d4e79983c27eca86b15
Adam Retter <adam.retter@googlemail.com>
2017-07-20 14:53:55 -0400

    Maven build of Fast Multi-Level Locks for Java

--- src/test/java/uk/ac/ic/doc/slurp/multilock/bank/BankTestMultiLock.java
+++ src/test/java/uk/ac/ic/doc/slurp/multilock/bank/BankTestMultiLock.java
@@ -24,14 +24,14 @@
  * SUCH DAMAGE.
  */
 
-package test.bank;
+package uk.ac.ic.doc.slurp.multilock.bank;
 
 import java.io.FileNotFoundException;
 import java.util.Random;
 
-public class BankTestRWLock extends BankTest {
+public class BankTestMultiLock extends BankTest {
 
-    public BankTestRWLock(int nWithdraw, int nDeposit, int nBranch, int nBank) {
+    public BankTestMultiLock(int nWithdraw, int nDeposit, int nBranch, int nBank) {
         super(nWithdraw, nDeposit, nBranch, nBank);
     }
 
@@ -40,17 +40,17 @@ public class BankTestRWLock extends BankTest {
         int acctId = r.nextInt(NUM_ACCTS_PER_BRANCH);
         int amt = r.nextInt(60);
 
-        b.rlock.lock();
+        b.mlock.lockIntentionWrite();
         Branch branch = b.branches[branchId];
-        branch.rlock.lock();
+        branch.mlock.lockIntentionWrite();
         Account acct = branch.accounts[acctId];
-        acct.wlock.lock();
+        acct.mlock.lockWrite();
         
         acct.withdraw(amt);
 
-        acct.wlock.unlock();
-        branch.rlock.unlock();
-        b.rlock.unlock();
+        acct.mlock.unlockWrite();
+        branch.mlock.unlockIntentionWrite();
+        b.mlock.unlockIntentionWrite();
     }
     
     public void deposit(Bank b, Random r) {
@@ -58,71 +58,46 @@ public class BankTestRWLock extends BankTest {
         int acctId = r.nextInt(NUM_ACCTS_PER_BRANCH);
         int amt = r.nextInt(60);
 
-        b.rlock.lock();
+        b.mlock.lockIntentionWrite();
         Branch branch = b.branches[branchId];
-        branch.rlock.lock();
+        branch.mlock.lockIntentionWrite();
         Account acct = branch.accounts[acctId];
-        acct.wlock.lock();
+        acct.mlock.lockWrite();
         
         acct.deposit(amt);
 
-        acct.wlock.unlock();
-        branch.rlock.unlock();
-        b.rlock.unlock();
+        acct.mlock.unlockWrite();
+        branch.mlock.unlockIntentionWrite();
+        b.mlock.unlockIntentionWrite();
     }
     
     public void sumOneBranch(Bank b, Random r) {
         int branchId = r.nextInt(NUM_BRANCHES);
         
-        b.rlock.lock();
+        b.mlock.lockIntentionRead();
         Branch branch = b.branches[branchId];
-        branch.rlock.lock();
-        
-        for (int i=0; i<NUM_ACCTS_PER_BRANCH; i++) {
-            branch.accounts[i].rlock.lock();
-        }
+        branch.mlock.lockRead();
         
         branch.sumBalances();
 
-        for (int i=0; i<NUM_ACCTS_PER_BRANCH; i++) {
-            branch.accounts[i].rlock.unlock();
-        }
-        
-        branch.rlock.unlock();
-        b.rlock.unlock();
+        branch.mlock.unlockRead();
+        b.mlock.unlockIntentionRead();
     }
     
     public void sumAll(Bank b) {
-        b.rlock.lock();
-        
-        // lock all branches (and all accounts in branches)
-        for (int i=0; i<NUM_BRANCHES; i++) {
-            Branch branch = b.branches[i];
-            branch.rlock.lock();
-            for (int j=0; j<NUM_ACCTS_PER_BRANCH; j++) {
-                branch.accounts[j].rlock.lock();
-            }
-        }
-        
+        b.mlock.lockRead();
+
         b.sumAll();
         
-        // unlock all branches (and all accounts in branches)
-        for (int i=0; i<NUM_BRANCHES; i++) {        
-            Branch branch = b.branches[i];
-            for (int j=0; j<NUM_ACCTS_PER_BRANCH; j++) {
-                branch.accounts[j].rlock.unlock();
-            }
-            branch.rlock.unlock();
-        }
-
-        b.rlock.unlock();
+        b.mlock.unlockRead();
     }
+
     public static void main(String[] args) throws FileNotFoundException, InterruptedException {
         int numWithdraw = Integer.parseInt(args[0]);
         int numDeposit = Integer.parseInt(args[1]);
         int numBranch = Integer.parseInt(args[2]);
         int numBank = Integer.parseInt(args[3]);
-        BankTest b = new BankTestRWLock(numWithdraw, numDeposit, numBranch, numBank);
+        BankTest b = new BankTestMultiLock(numWithdraw, numDeposit, numBranch, numBank);
         b.runExperiment();
     }
     
